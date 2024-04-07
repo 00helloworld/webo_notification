2f46f9637c1d813e68f46b711db13516b76b61f0
yanshen <yanshen0406@gmail.com>
2024-04-06 21:43:02 -0500

    optimize

--- src/crawler.py
+++ src/crawler.py
@@ -22,6 +22,7 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--ignore-certificate-errors")
+chrome_options.add_argument("blink-settings=imagesEnabled=false")
 
 # 创建 Chrome WebDriver
 # 注意：需要提前下载对应版本的 ChromeDriver，并指定其路径
--- test.py
+++ test.py
@@ -22,6 +22,7 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--ignore-certificate-errors")
+chrome_options.add_argument("blink-settings=imagesEnabled=false")
 
 
 def save_html(driver, name='tmp'):
